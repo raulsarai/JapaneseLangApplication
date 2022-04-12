c543f3b92745bf354cf8c31d807531fe566c8961
raulsarai <raulmauro.sj@gmail.com>
2022-04-12 14:12:58 -0300

    SplashScreenActivity added; manifest.xml and gradle.xml file has updated;

---
+++ app/src/main/java/br/com/japaneselangapplication/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package br.com.japaneselangapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
+
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 7000);
+
+
+    }
+}
